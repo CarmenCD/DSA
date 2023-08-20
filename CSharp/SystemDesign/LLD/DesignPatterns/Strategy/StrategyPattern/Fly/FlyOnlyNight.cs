83cf65d138b36b928288ce376d83a064a3788839
Carmen-Gabriela Lup <carmen.gabriela.lup@gmail.com>
2023-08-20 10:35:30 +0300

    Duck simulation game - initial design and limitations - final solution strategy pattern

---
+++ CSharp/SystemDesign/LLD/DesignPatterns/Strategy/StrategyPattern/Fly/FlyOnlyNight.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Strategy.StrategyPattern.Fly
+{
+    public class FlyOnlyNight : IFlyable
+    {
+        public void Fly()
+        {
+            Console.WriteLine("Night fly is romantic");
+        }
+    }
+}
---
+++ CSharp/SystemDesign/LLD/DesignPatterns/Strategy/StrategyPattern/Quack/QuackLoud.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPatterns.Strategy.StrategyPattern.Quack
+{
+    public class QuackLoud : IQuack
+    {
+        public void Quack()
+        {
+            Console.WriteLine("Helllo... Am I quack loud enough?");
+        }
+    }
+}
